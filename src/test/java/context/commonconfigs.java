3a0a4facea8800badf735677fc4b0f8ff1b24c2b
kdhand2 <karthikdhandapani03@gmail.com>
2024-12-08 16:31:31 +0530

    Project created

---
+++ src/test/java/context/commonconfigs.java
@@ -0,0 +1,69 @@
+package context;
+
+import com.google.gson.JsonObject;
+import com.microsoft.playwright.*;
+
+public class commonconfigs {
+
+    private Page page;
+    private String cdpUrl;
+    private Browser browser;
+    private BrowserContext context;
+    private Playwright playwright;
+    private BrowserType browserType;
+    private final String username = "kdhand2";
+    private final String accessKey = "CtSOjmjSWEH8nb5P1izMThNjbHdGVC2fcoZ2Is3HtKDlxAieCK";
+
+    public commonconfigs() {
+    }
+
+    public void openBrowser() {
+        playwright = Playwright.create();
+        lamdaSetup();
+        String browserTypeString = System.getProperty("BrowserType");
+        System.out.println("BrowserType: " + browserTypeString);
+        boolean headless = Boolean.parseBoolean(System.getProperty("headLess"));
+        browserType = getBrowserType(browserTypeString);
+        browser = browserType.connect(cdpUrl);
+        browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(headless));
+        context = browser.newContext();
+        page = context.newPage();
+        page.setViewportSize(1920, 1080);
+    }
+
+    private BrowserType getBrowserType(String browserTypeString) {
+        return switch (browserTypeString.toLowerCase()) {
+            case "chrome" -> playwright.chromium();
+            case "webkit" -> playwright.webkit();
+            case "firefox" -> playwright.firefox();
+            default -> throw new IllegalArgumentException("Browser not supported: " + browserTypeString);
+        };
+    }
+
+    public Page getPage() {
+        return page;
+    }
+
+    private void lamdaSetup() {
+        JsonObject capabilities = new JsonObject();
+        JsonObject ltOptions = new JsonObject();
+
+        capabilities.addProperty("browserName", "Chrome"); // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+        capabilities.addProperty("browserVersion", "latest");
+        ltOptions.addProperty("platform", "Windows 11");
+        ltOptions.addProperty("name", "Playwright Test");
+        ltOptions.addProperty("build", "Playwright Java Build");
+        ltOptions.addProperty("user", username);
+        ltOptions.addProperty("accessKey", accessKey);
+        capabilities.add("LT:Options", ltOptions);
+
+        cdpUrl = "wss://cdp.lambdatest.com/playwright?capabilities=" + capabilities;
+    }
+
+    public void teardown() {
+        page.close();
+        context.close();
+        browser.close();
+        playwright.close();
+    }
+}
---
+++ src/test/java/steps/lamdaTestSteps.java
@@ -0,0 +1,97 @@
+package steps;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.BoundingBox;
+import pages.LamdaTestPages;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class lamdaTestSteps {
+
+
+    String variable;
+    String validationMessage;
+    LamdaTestPages lamdaTestPages ;
+    Page page;
+
+    public lamdaTestSteps(Page page) {
+        lamdaTestPages = new LamdaTestPages();
+        this.page = page;
+    }
+
+    public void selectStageFromForm(String val) {
+        page.locator("//a[contains(text(),'" + val + "')]").click();
+    }
+
+    public void validateForm(String formname) {
+        String url = page.url();
+        String endpoint = url.substring(47);
+        assertEquals(endpoint, formname);
+    }
+
+    public void createVariable(String val) {
+        variable = val;
+    }
+
+    public void enterTheValueInBox() {
+        page.locator("//input[@id=\"user-message\"]").fill(variable);
+    }
+
+    public void clickGetChecked() {
+        page.click("[id=\"showInput\"]");
+    }
+
+    public void validateVariable() {
+        assertEquals(page.locator("[id=\"message\"]").textContent(), variable);
+    }
+
+
+    public void checkValueAndSelect(String defaultValue, String expectedValuew) {
+        Locator slider = page.locator("//*[@id=\"slider3\"]/div/input");
+        BoundingBox box = slider.boundingBox();
+        double startX = box.x + box.width / 2;
+        double startY = box.y + box.height / 2;
+        page.mouse().move(startX, startY);
+        page.mouse().down();
+        page.mouse().move(startX + 215, startY);
+        page.mouse().up();
+    }
+
+    public void validateRange(String expectedRange) {
+        String actualRange = page.locator("[id=\"rangeSuccess\"]").textContent();
+        assert actualRange.equals(expectedRange) : "Range Slider value is not set correctly, expected 95 but got " + actualRange;
+    }
+
+
+    public void clickOnSubmit() {
+        Locator inputField = page.locator("[id=\"name\"]");
+        page.locator("//button[contains(text(),\"Submit\")]").click();
+        validationMessage = (String) inputField.evaluate("el => el.validationMessage");
+    }
+
+    public void validateErrorMesssage(String message) {
+        assertEquals( validationMessage, message);
+    }
+
+    public void fillManditoryParams() {
+        page.locator("[id=\"name\"]").fill("karthik");
+        page.locator("[id=\"inputEmail4\"]").fill("karthik@mail.com");
+        page.locator("[id=\"inputPassword4\"]").fill("karthik@123");
+        page.locator("[id=\"company\"]").fill("lamda");
+        page.locator("[id=\"websitename\"]").fill("test");
+        page.locator("[id=\"inputCity\"]").fill("hyd");
+        page.locator("[id=\"inputAddress1\"]").fill("hyd1");
+        page.locator("[id=\"inputAddress2\"]").fill("hyd2");
+        page.locator("[id=\"inputState\"]").fill("usa");
+        page.locator("[name=\"country\"]").selectOption("US");
+        page.locator("[id='inputZip']").fill("12321");
+    }
+
+    public void validateSucessMessage(String sucessessage) {
+        assertEquals((page.locator("[class=\"success-msg hidden\"]")).textContent(), sucessessage);
+    }
+
+
+}
